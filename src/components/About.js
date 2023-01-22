91f66dea6cc667de3b49c6abb4fddaf462b0e782
Aditya Mishra <112647556+AdiMiMC07@users.noreply.github.com>
2023-01-22 10:56:02 +0530

    Update About.js

--- src/components/About.js
+++ src/components/About.js
@@ -7,7 +7,7 @@ function About() {
         This webapp can be used to get the current weather data and 8-day daily forecast data of your city 
         <br />This app is made with <kbd>Reactjs</kbd> and <kbd>Bootstrap</kbd>
         <br />
-        <p style={{textDecorationLine:"underline",margin:"2rem 0"}}>Note: The web app right now works for Indian cities only due to the hard limit...</p> 
+        <p style={{textDecorationLine:"underline",margin:"2rem 0"}}>Note: The web app right now works for Indian cities only.</p> 
     </div>
   )
 }
